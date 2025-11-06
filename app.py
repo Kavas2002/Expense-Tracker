4bafb270f6dcaa24263cbb0d062210dda591cda9
kavas2002 <kavaskar302@gmail.com>
2025-11-06 18:35:06 +0530

    auto detect time

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-from datetime import date, time
-
+from datetime import datetime, time,date
 st.title("Expense Tracker")
 st.write(" Enter the details of the expense")
 
 
 with st.form("Expense_form"):
     expense_date = st.date_input("date",date.today())
-    expense_time = st.time_input("Time",time())
+    expense_time = st.time_input("Time",datetime.now().time())
     category = st.selectbox("Select the category of expense",["Personal","Business","Friend","Debt","Other"])
     amount = st.number_input("Enter the Spent amount",min_value=0.0,format="%.2f")
     Descrition = st.text_input("enter the Product/Service description")
