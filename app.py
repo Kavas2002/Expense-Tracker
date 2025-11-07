e5d58c732ec0bb02c9dbc9a8b725c21bf427cc92
kavas2002 <kavaskar302@gmail.com>
2025-11-07 16:25:15 +0530

    Add submit button

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from datetime import datetime, time,date
+from datetime import datetime,date
+import pandas as pd
+
 st.title("Expense Tracker")
 st.write(" Enter the details of the expense")
 
@@ -10,3 +12,20 @@ with st.form("Expense_form"):
     category = st.selectbox("Select the category of expense",["Personal","Business","Friend","Debt","Other"])
     amount = st.number_input("Enter the Spent amount",min_value=0.0,format="%.2f")
     Descrition = st.text_input("enter the Product/Service description")
+    localtion = st.text
+    
+    checkbox = st.checkbox("confirm expense")
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        if checkbox:
+            st.success("expense recorded successfully")
+            Expense_data={
+                "Date":expense_date,
+                "Time":expense_time,
+                "Category":category,
+                "Amount":amount,
+                "Description":Descrition
+            }
+        else:
+            st.warring("please confirm the checkbox")
+        
